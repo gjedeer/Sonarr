ace89c6cff25086dd6688295c9dc4d7a09212ca8
Mark McDowall <markus.mcd5@gmail.com>
2017-02-10 22:47:11 -0800

    v3 UI

---
+++ frontend/src/Components/Link/IconButton.js
@@ -0,0 +1,43 @@
+import React, { PropTypes } from 'react';
+import Icon from 'Components/Icon';
+import Link from './Link';
+import styles from './IconButton.css';
+
+function IconButton(props) {
+  const {
+    className,
+    iconClassName,
+    name,
+    kind,
+    size,
+    ...otherProps
+  } = props;
+
+  return (
+    <Link
+      className={className}
+      {...otherProps}
+    >
+      <Icon
+        className={iconClassName}
+        name={name}
+        kind={kind}
+        size={size}
+      />
+    </Link>
+  );
+}
+
+IconButton.propTypes = {
+  className: PropTypes.string.isRequired,
+  iconClassName: PropTypes.string,
+  kind: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  size: PropTypes.number
+};
+
+IconButton.defaultProps = {
+  className: styles.button
+};
+
+export default IconButton;

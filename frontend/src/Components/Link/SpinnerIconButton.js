c1d11b32345bdd9c7cf9eb4626ab01eac1e16680
Mark McDowall <markus.mcd5@gmail.com>
2017-02-10 22:47:11 -0800

    v3 UI

---
+++ frontend/src/Components/Link/SpinnerIconButton.js
@@ -0,0 +1,36 @@
+import React, { PropTypes } from 'react';
+import { icons } from 'Helpers/Props';
+import IconButton from './IconButton';
+
+function SpinnerIconButton(props) {
+  const {
+    name,
+    spinningName,
+    isDisabled,
+    isSpinning,
+    ...otherProps
+  } = props;
+
+  return (
+    <IconButton
+      name={isSpinning ? `${spinningName || name} fa-spin` : name}
+      isDisabled={isDisabled || isSpinning}
+      {...otherProps}
+    />
+  );
+}
+
+SpinnerIconButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  spinningName: PropTypes.string.isRequired,
+  isDisabled: PropTypes.bool.isRequired,
+  isSpinning: PropTypes.bool.isRequired
+};
+
+SpinnerIconButton.defaultProps = {
+  spinningName: icons.SPINNER,
+  isDisabled: false,
+  isSpinning: false
+};
+
+export default SpinnerIconButton;
